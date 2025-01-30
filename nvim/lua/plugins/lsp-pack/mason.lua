1f8cda409c4764c2fbfcb8cc20ba155d0623afe6
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-01-30 03:36:41 +0100

    oops

--- nvim/lua/plugins/lsp-pack/mason.lua
+++ nvim/lua/plugins/lsp-pack/mason.lua
@@ -2,60 +2,60 @@
 return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-	-- config = function()
+	config = function()
 
-	-- 	local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-	-- 	local lspconfig = require('lspconfig')
+		local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+		local lspconfig = require('lspconfig')
 
-	-- 	local handlers =  {
-	-- 		["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {
-	-- 			border = "single",
-	-- 		}),
-	-- 		["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {
-	-- 			border = "single",
-	-- 		}),
-	-- 	}
+		local handlers =  {
+			["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {
+				border = "single",
+			}),
+			["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {
+				border = "single",
+			}),
+		}
 
-	-- 	require("mason").setup({
-	-- 		ui = {
-	-- 			icons = {
-	-- 				package_installed = "✓",
-	-- 				package_pending = "➜",
-	-- 				package_uninstalled = "✗"
-	-- 			}
-	-- 		}
-	-- 	})
-	-- 	require('mason-lspconfig').setup({
-	-- 		-- ensure_installed = {'rust_analyzer'},
-	-- 		handlers = {
-	-- 			function(server_name)
-	-- 				lspconfig[server_name].setup({
-	-- 					capabilities = lsp_capabilities,
-	-- 					handlers = handlers,
-	-- 				})
-	-- 			end,
-	-- 			lua_ls = function()
-	-- 				lspconfig.lua_ls.setup({
-	-- 					capabilities = lsp_capabilities,
-	-- 					handlers = handlers,
-	-- 					settings = {
-	-- 						Lua = {
-	-- 							runtime = {
-	-- 								version = 'LuaJIT'
-	-- 							},
-	-- 							diagnostics = {
-	-- 								globals = {'vim'},
-	-- 							},
-	-- 							workspace = {
-	-- 								library = {
-	-- 									vim.env.VIMRUNTIME,
-	-- 								}
-	-- 							}
-	-- 						}
-	-- 					}
-	-- 				})
-	-- 			end,
-	-- 		}
-	-- 	})
-	-- end
+		require("mason").setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗"
+				}
+			}
+		})
+		require('mason-lspconfig').setup({
+			-- ensure_installed = {'rust_analyzer'},
+			handlers = {
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = lsp_capabilities,
+						handlers = handlers,
+					})
+				end,
+				lua_ls = function()
+					lspconfig.lua_ls.setup({
+						capabilities = lsp_capabilities,
+						handlers = handlers,
+						settings = {
+							Lua = {
+								runtime = {
+									version = 'LuaJIT'
+								},
+								diagnostics = {
+									globals = {'vim'},
+								},
+								workspace = {
+									library = {
+										vim.env.VIMRUNTIME,
+									}
+								}
+							}
+						}
+					})
+				end,
+			}
+		})
+	end
 }
