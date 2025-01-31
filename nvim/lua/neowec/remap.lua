6f10a1a8a8a5feaf90c706bce456ac09c7b5f096
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-01-31 13:08:03 +0100

    cleaner remap.lua + <leader>wo remap for :only

--- nvim/lua/neowec/remap.lua
+++ nvim/lua/neowec/remap.lua
@@ -1,34 +1,48 @@
 vim.g.mapleader = " "
 
--- colemak better motion 
+
+--			COLEMAK BETTER MOTION 
 vim.keymap.set({"n","v"}, "n","j", {noremap = true})
 vim.keymap.set({"n","v"}, "e","k", {noremap = true})
 vim.keymap.set({"n","v"}, "i","l", {noremap = true})
-
 vim.keymap.set({"n","v"}, "l","i", {noremap = true})
--- vim.keymap.set({"n","v","i"}, "<leader><leader>","<cr>")
+
+
+
+
+
 vim.keymap.set("n", "<leader>e", function()
     vim.cmd.Neotree("reveal")
 end)
--- better escape 
-vim.keymap.set({"n","v","i"}, "<leader>x","<ESC>")
--- better undo
-vim.keymap.set({"n"},"U","<C-r>")
--- no highlight
-vim.keymap.set({"n","v"}, "<leader>no",vim.cmd.nohl)
--- tabs
+
+
+
+--			TABS
 vim.keymap.set({"n"}, "<leader>tn",vim.cmd.tabnew)
 vim.keymap.set({"n"}, "<leader>tc",vim.cmd.tabclose)
 vim.keymap.set({"n"}, "<leader>ti",vim.cmd.tabnext)
 vim.keymap.set({"n"}, "<leader>th",vim.cmd.tabprevious)
--- split
-vim.keymap.set({"n"}, "<leader>sv",vim.cmd.vsplit)
-vim.keymap.set({"n"}, "<leader>sh",vim.cmd.split)
--- windows motion
+
+--			WINDOWS MOTION
 vim.keymap.set({"n","v"}, "<leader>wn","<C-W>j")
 vim.keymap.set({"n","v"}, "<leader>we","<C-W>k")
 vim.keymap.set({"n","v"}, "<leader>wi","<C-W>l")
 vim.keymap.set({"n","v"}, "<leader>wh","<C-W>h")
+vim.keymap.set({"n","v"}, "<leader>wo",vim.cmd.only)
 
--- mass rename
+--			SPLIT
+vim.keymap.set({"n"}, "<leader>sv",vim.cmd.vsplit)
+vim.keymap.set({"n"}, "<leader>sh",vim.cmd.split)
+
+
+
+
+
+--			BETTER ESCAPE 
+vim.keymap.set({"n","v","i"}, "<leader>x","<ESC>")
+--			BETTER UNDO
+vim.keymap.set({"n"},"U","<C-r>")
+--			NO HIGHLIGHT
+vim.keymap.set({"n","v"}, "<leader>no",vim.cmd.nohl)
+--			mass rename
 vim.keymap.set("n", "<leader>vrn", ":IncRename ")
