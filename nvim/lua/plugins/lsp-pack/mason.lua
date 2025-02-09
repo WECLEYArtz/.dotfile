39f0882c9448ac3903ccc21daf73c1f2e2491cc9
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-02-09 21:34:55 +0100

    redone lsp

--- nvim/lua/plugins/lsp-pack/mason.lua
+++ nvim/lua/plugins/lsp-pack/mason.lua
@@ -1,63 +1,29 @@
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
+	'williamboman/mason-lspconfig.nvim',
+	dependencies = {
+		"williamboman/mason.nvim",
+		'hrsh7th/cmp-nvim-lsp',
+	},
+	config = function()
 
-			local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-			local lspconfig = require('lspconfig')
+		local capabilities = require('cmp_nvim_lsp').default_capabilities()
+		local handlers =  {
+				border = "single",
+		}
 
-			local handlers =  {
-				["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {
-					border = "single",
-				}),
-				["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {
-					border = "single",
-				}),
-			}
-
-			require("mason").setup({
-				ui = {
-					border = "single",
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗"
-					}
+		require("mason").setup()
+		require("mason-lspconfig").setup_handlers {
+			function (server_name) -- default handler (optional)
+				require("lspconfig")[server_name].setup
+				{
+					capabilities = capabilities,
+					handlers = handlers,
 				}
-			})
-			require('mason-lspconfig').setup({
-				-- ensure_installed = {'rust_analyzer'},
-				handlers = {
-					function(server_name)
-						lspconfig[server_name].setup({
-							capabilities = lsp_capabilities,
-							handlers = handlers,
-						})
-					end,
-					lua_ls = function()
-						lspconfig.lua_ls.setup({
-							capabilities = lsp_capabilities,
-							handlers = handlers,
-							settings = {
-								Lua = {
-									runtime = {
-										version = 'LuaJIT'
-									},
-									diagnostics = {
-										globals = {'vim'},
-									},
-									workspace = {
-										library = {
-											vim.env.VIMRUNTIME,
-										}
-									}
-								}
-							}
-						})
-					end,
-				}
-			})
-		end
-	}
+			end,
+
+			-- ["rust_analyzer"] = function ()
+			-- 	require("rust-tools").setup {}
+			-- end
+		}
+	end,
 }
--- nvim/lua/plugins/lsp-pack/nvim-lsp.lua
+++ nvim/lua/plugins/lsp-pack/nvim-lsp.lua
@@ -1,61 +1,10 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"hrsh7th/nvim-cmp",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-		},
-		config = function()
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"folke/lazydev.nvim",
+	},
+	config = function()
 
-			vim.api.nvim_create_autocmd('LspAttach', {
-
-				group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
-				callback = function(event)
-					local opts = {buffer = event.buf}
-
-					vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-					vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-
-					vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-					vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-
-					vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-					vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-
-					vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-					vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-					-- vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
-
-					vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
-				end,
-			})
-
-			local cmp = require('cmp')
-			cmp.setup({
-				-- unfinished border fixing, its unhinged mess
-				window = {
-					completion = vim.tbl_extend("force", cmp.config.window.bordered(), {
-						-- border="single",
-						winhighlight = "NormalFloat:None,FloatBorder:None,CursorLine:Visual,Search:None",
-					}),
-					documentation = vim.tbl_extend("force", cmp.config.window.bordered(), {
-						border="single",
-						winhighlight = "NormalFloat:None,FloatBorder:None,CursorLine:Visual,Search:None",
-					}),
-				},
-				sources = cmp.config.sources({
-					{name = 'nvim_lsp'},
-					{name = 'luasnip'},
-				}, {
-					{name = 'buffer'},
-				}),
-				snippet = {
-					expand = function(args)
-						require('luasnip').lsp_expand(args.body)
-					end,
-				},
-			})
-		end,
-	}
+		require("lspconfig").lua_ls.setup({})
+	end
 }
