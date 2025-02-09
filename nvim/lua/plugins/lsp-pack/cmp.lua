2224c007b213fc6f6016c348021b9f2dac80e242
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-02-09 23:35:03 +0100

    redone theming

--- nvim/lua/plugins/lsp-pack/cmp.lua
+++ nvim/lua/plugins/lsp-pack/cmp.lua
@@ -11,7 +11,10 @@ return
 	config = function()
 		-- Set up nvim-cmp.
 		local cmp = require'cmp'
-
+		local cmp_theme = vim.tbl_extend("force", cmp.config.window.bordered(), {
+			border="single",
+			winhighlight = "NormalFloat:None,FloatBorder:None,CursorLine:Visual,Search:None",
+		})
 		cmp.setup({
 			snippet = {
 				-- REQUIRED - you must specify a snippet engine
@@ -30,15 +33,15 @@ return
 				end,
 			},
 			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
+				completion = cmp_theme,
+				documentation = cmp_theme,
 			},
 			mapping = cmp.mapping.preset.insert({
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
 				['<C-f>'] = cmp.mapping.scroll_docs(4),
 				['<C-Space>'] = cmp.mapping.complete(),
 				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
@@ -71,14 +74,14 @@ return
 		})
 
 		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-		cmp.setup.cmdline(':', {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = 'path' }
-			}, {
-				{ name = 'cmdline' }
-			}),
-			matching = { disallow_symbol_nonprefix_matching = false }
-		})
+		-- cmp.setup.cmdline(':', {
+		-- 	mapping = cmp.mapping.preset.cmdline(),
+		-- 	sources = cmp.config.sources({
+		-- 		{ name = 'path' }
+		-- 	}, {
+		-- 		{ name = 'cmdline' }
+		-- 	}),
+		-- 	matching = { disallow_symbol_nonprefix_matching = false }
+		-- })
 	end
 }
--- nvim/lua/plugins/lsp-pack/mason.lua
+++ nvim/lua/plugins/lsp-pack/mason.lua
@@ -8,7 +8,12 @@ return {
 
 		local capabilities = require('cmp_nvim_lsp').default_capabilities()
 		local handlers =  {
+			["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {
 				border = "single",
+			}),
+			["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {
+				border = "single",
+			}),
 		}
 
 		require("mason").setup()
