a46f082e71cb0aa2ae8dd3f51521eb87d93241fd
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-03-25 14:04:23 +0000

    qof dashboard feature

--- nvim/lua/plugins/themeing-pack/alpha.lua
+++ nvim/lua/plugins/themeing-pack/alpha.lua
@@ -12,12 +12,15 @@
 
 return {
 	"goolord/alpha-nvim",
-	dependencies = { 'echasnovski/mini.icons' },
+	dependencies = { "echasnovski/mini.icons" },
 	-- dependencies = { 'nvim-tree/nvim-web-devicons' },
 	config = function()
+		local vf = vim.fn --less headache
 		local theta = require("alpha.themes.theta")
 		local dashboard = require("alpha.themes.dashboard")
-		local config = vim.fn.stdpath('config')
+		local config = vf.resolve(vf.stdpath("config"))
+		local path_old = ""
+		local log = vim.print
 
 		-- -- available: devicons, mini, default is mini
 		-- -- if provider not loaded and enabled is true, it will try to use another provider
@@ -27,14 +30,30 @@ return {
 			-- dashboard.button("SPC f f", "󰈞  Find file"),
 			-- dashboard.button("SPC f g", "󰊄  Live grep"),
 
-			dashboard.button("c", "  Configuration", "<cmd>Neotree "..config.."<CR>"),
-			dashboard.button("m", "└─   remaping", "<cmd>e "..config.."/lua/neowec/remap.lua<CR>"),
+			dashboard.button("c", "  Configuration", function()
+				if path_old == "" then
+					if vf.getcwd() == config then
+						log("[Alpha]: Already in config dir")
+						return
+					end
+					path_old = vf.getcwd()
+				end
+
+				if vf.getcwd() == config then
+					vim.cmd("cd " .. path_old)
+					log("[Alpha]: Reverted back to " .. path_old .. " ")
+				else
+					vim.cmd("cd " .. config)
+					log("[Alpha]: Switched to Configurations directory  ")
+				end
+			end),
+			dashboard.button("m", "└─   remaping", "<cmd>e " .. config .. "/lua/neowec/remap.lua<CR>"),
 			dashboard.button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
 			dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
 		}
 		require("alpha").setup(
-		theta.config
-		-- dashboard.config
+			theta.config
+			-- dashboard.config
 		)
 	end,
 }
